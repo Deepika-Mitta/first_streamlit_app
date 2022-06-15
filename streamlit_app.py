446d26afdd102e15d99b0b7dac9b764021962121
Deepika-Mitta <107551056+Deepika-Mitta@users.noreply.github.com>
2022-06-15 17:43:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ streamlit.text("🥗 Kale, Spinach & Rocket Smoothie")
 
 streamlit.text("🐔 Hard-Boiled Free-Range Egg")
 
-streamlit.text('🥑🍞 Avacado Toast')
+streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
