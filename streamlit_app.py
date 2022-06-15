334f43d9fd66f07109a545111f25949a1240de03
Deepika-Mitta <107551056+Deepika-Mitta@users.noreply.github.com>
2022-06-15 16:52:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,5 @@ streamlit.title("My Parents New Healthy Diner")
 
 streamlit.header("Breakfast Menu")
 
-streamlit.caption("omega 3 & Blueberry Oatmeal""Kale")
+streamlit.caption("omega 3 & Blueberry Oatmeal"
+ "Kale")
