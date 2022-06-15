c9d1e8df87d54ec135cddcfe4a427704d21e7a1d
Deepika-Mitta <107551056+Deepika-Mitta@users.noreply.github.com>
2022-06-15 16:55:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,8 @@ streamlit.title("My Parents New Healthy Diner")
 
 streamlit.header("Breakfast Menu")
 
-streamlit.caption("omega 3 & Blueberry Oatmeal"
- "Kale")
+streamlit.caption("omega 3 & Blueberry Oatmeal")
+
+streamlit.caption("Kale, Spinach & Rocket Smoothie")
+
+streamlit.caption("Hard-Boiled Free-Range Egg")
